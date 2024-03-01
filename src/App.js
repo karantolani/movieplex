e6b715ee54342a006545e71a8105f1b46e2f79aa
Karan Tolani <54103880+karantolani@users.noreply.github.com>
2024-03-01 16:24:10 +0530

    Change in App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import RootLayout from "./pages/RootLayout";
+import Home from "./pages/Home";
+import MovieDetail from "./pages/MovieDetail";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            },
+            {
+                path: "/movie/:imdbID",
+                element: <MovieDetail />
+            }
+        ]
+    }
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <RouterProvider router={router} />
   );
 }
 
